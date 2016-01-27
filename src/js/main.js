d12ce57ba5e94dff1a680a43737d42b9cb01016e
Daniel Klein <danhirschklein@gmail.com>
2016-01-27 13:25:30 -0700

    Added cardInfo object

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ $(document).on('ready', function() {
   console.log('sanity check!');
   Stripe.setPublishableKey('pk_test_2n6HOH3q1Hod9NkFXdTKihY8');
 });
+
+var cardInfo = {
+  number: $('.card-number').val(),
+  cvc: $('.card-cvc').val(),
+  exp_month: $('.card-expiry-month').val().split('/')[0],
+  exp_year: $('.card-expiry-year').val().split('/')[1]
+};
+
+Stripe.card.createToken(cardInfo, stripeResponseHandler);
